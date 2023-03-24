ca6978034e247488fe896aa0c4703fe1e2a5e5f6
madchristian <christianstrube@icloud.com>
2023-03-24 23:28:28 +0100

    .

--- Dockerfile
+++ Dockerfile
@@ -7,16 +7,16 @@ RUN apt-get update && apt-get install -y git
 WORKDIR /usr/local/apache2/htdocs/
 
 # Copy the files from the temporary directory to the target directory
-COPY . /usr/local/apache2/htdocs/
+#COPY . /usr/local/apache2/htdocs/
 
 # Update httpd.conf
-RUN sed -i 's/Options Indexes FollowSymLinks/Options Indexes FollowSymLinks\nAddType application\/javascript .js/' /usr/local/apache2/conf/httpd.conf
-RUN echo 'ServerName parken.cstrube.de' >> /usr/local/apache2/conf/httpd.conf
-RUN echo 'AddType text/css .css' >> /usr/local/apache2/conf/httpd.conf
+#RUN sed -i 's/Options Indexes FollowSymLinks/Options Indexes FollowSymLinks\nAddType application\/javascript .js/' /usr/local/apache2/conf/httpd.conf
+#RUN echo 'ServerName parken.cstrube.de' >> /usr/local/apache2/conf/httpd.conf
+#RUN echo 'AddType text/css .css' >> /usr/local/apache2/conf/httpd.conf
 # Update httpd.conf for CORS^
-RUN echo 'Header set Access-Control-Allow-Origin "*"' >> /usr/local/apache2/conf/httpd.conf
-RUN echo 'Header set Access-Control-Allow-Methods "GET,POST,OPTIONS,DELETE,PUT"' >> /usr/local/apache2/conf/httpd.conf
-RUN echo 'Header set Access-Control-Allow-Headers "Content-Type, Authorization, X-Requested-With"' >> /usr/local/apache2/conf/httpd.conf
+#RUN echo 'Header set Access-Control-Allow-Origin "*"' >> /usr/local/apache2/conf/httpd.conf
+#RUN echo 'Header set Access-Control-Allow-Methods "GET,POST,OPTIONS,DELETE,PUT"' >> /usr/local/apache2/conf/httpd.conf
+#RUN echo 'Header set Access-Control-Allow-Headers "Content-Type, Authorization, X-Requested-With"' >> /usr/local/apache2/conf/httpd.conf
 
 # Start Apache
 CMD ["httpd-foreground"]
--- src/js/scanlicenseplate.js
+++ src/js/scanlicenseplate.js
@@ -1,4 +1,5 @@
-import { captureCamera } from 'tesseract-with-html5-camera';
+import Tesseract from 'tesseract.js';
+
 import { showSpinner, hideSpinner } from './spinner.js';
 import { getLocation } from './positionandsave.js';
 
@@ -7,37 +8,29 @@ export const imageElement = document.getElementById('licensePlateImage');
 
 // function to scan image for license plate text
 export async function scanImage() {
-  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-    console.error('Camera not supported');
+  if (fileInput.files.length === 0) {
+    console.error('No image selected');
     return;
   }
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-    const image = await captureCamera(stream);
-    showSpinner(); // display loading spinner
-    // use Tesseract OCR to convert image to text
-    Tesseract.recognize(image, {
-      lang: 'deu', // set language to German
-      tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜ0123456789', // only allow these characters
-      tessedit_pageseg_mode: 10, // adjust OCR window size
-      lstm_choice_mode: 1, // use lstm training model
-    }).then(result => {
-      console.log(result.text); // log recognized text
-      hideSpinner(); // hide loading spinner
-      const licensePlate = result.text;
-      getLocation(licensePlate); // call getLocation() function with license plate
-      // process recognized text further, e.g. save it in a database or apply further text processing
-    }).catch(error => {
-      console.error(error);
-      hideSpinner(); // hide loading spinner
-    }).finally(() => {
-      stream.getVideoTracks()[0].stop();
-    });
-  } catch (error) {
+  const imageFile = fileInput.files[0];
+  showSpinner(); // display loading spinner
+  // use Tesseract OCR to convert image to text
+  Tesseract.recognize(imageFile, {
+    lang: 'deu', // set language to German
+    tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜ0123456789', // only allow these characters
+    tessedit_pageseg_mode: 10, // adjust OCR window size
+    lstm_choice_mode: 1, // use lstm training model
+  }).then(result => {
+    console.log(result.text); // log recognized text
+    hideSpinner(); // hide loading spinner
+    const licensePlate = result.text;
+    getLocation(licensePlate); // call getLocation() function with license plate
+    // process recognized text further, e.g. save it in a database or apply further text processing
+  }).catch(error => {
     console.error(error);
-  }
+    hideSpinner(); // hide loading spinner
+  });
 }
-
 export function processImage(input) {
   const file = input.files[0];
   const reader = new FileReader();
